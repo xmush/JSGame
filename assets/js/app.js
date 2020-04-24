a94a5ec354e8235889ca9170130cef23913c3745
xmush <sholeh@alterra.id>
2020-04-24 17:17:03 -0400

    add changes board grid, marker and player location

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,60 @@
 // let mainDiv = document.getElementById("main")
-var mainDiv = document.getElementById("main")
-// console.log(mainDiv)
-// const listMiniBox = []
-for(i=0; i<100; i++) {
-    divBox = document.createElement("div");
-    divBox.setAttribute("class", "miniBox");
-    boxContent = document.createTextNode(`box`)
-    divBox.appendChild(boxContent)
-    mainDiv.appendChild(divBox)
+const mainDiv = document.getElementById("main")
+const listColor = ['#c2b9dd', '#ccc7d4', '#faf6ff', '#c4dad7', '#f5d9d1', '#f3bced', '#a38da4', '#c6e1c1', '#ded8dd', '#b9b1a4', '#ad888c', '#ffd7c9', '#93c2d3', '#bef2f6', '#e0e3dd', '#a3c6ce', '#b4edf3', '#e4fec5', '#f9db7c', '#ecd7d9', '#cccccc', '#d1d8de', '#d7eef9', '#fae1cc', '#f6d7cb', '#b4a598']
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
 }
+// console.log(mainDiv)
+const listMiniBox = []
+let numberBox = 1
+
+for(i=0; i<10; i++) {
+    let tempList = []
+    for(j=0; j<10; j++) {
+        divBox = document.createElement("div");
+        divBox.setAttribute("class", "miniBox");
+        // divBox.setAttribute("id", `col${i}row${j}`);
+        divBox.setAttribute("id", `${numberBox}`);
+        divBox.style.backgroundColor = listColor.random()
+        // boxContent = document.createTextNode(`${i}${j}`)
+        boxContent = document.createTextNode('')
+        divBox.appendChild(boxContent)
+
+        // mainDiv.appendChild(divBox)
+        tempList.push(divBox)
+        numberBox++
+    }
+    listMiniBox.push(tempList)
+}
+
+for(itemCol of listMiniBox.reverse()) {
+    idxOfItem = listMiniBox.indexOf(itemCol) +1
+    if(idxOfItem % 2 != 0) {
+        // console.log(idxOfItem)
+        for(itemRow of itemCol.reverse()) {
+            mainDiv.appendChild(itemRow)
+        }
+    } else {
+        for(itemRow of itemCol) {
+            mainDiv.appendChild(itemRow)
+        }
+    }
+}
+
+// console.log(...listMiniBox)
+
+// return location
+function addPlayer() {
+    playerLocation = document.getElementById('1')
+    playerLocation2 = document.getElementById('2')
+
+    playerMarker = document.createElement("div")
+    playerMarker.setAttribute("class", "marker")
+    playerMarker2 = document.createElement("div")
+    playerMarker2.setAttribute("class", "marker2")
+    playerLocation.appendChild(playerMarker)
+    playerLocation2.appendChild(playerMarker2)
+    // console.log(playerLocation)
+}
+
+addPlayer()
