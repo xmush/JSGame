fff27248f099ebea6307a7a79f74052615b423e5
ahmadajip55 <pangestu@alterra.id>
2020-04-25 10:34:00 -0400

    edit layout by aji

--- assets/js/app.js
+++ assets/js/app.js
@@ -63,59 +63,66 @@ addPlayer()
 // Aji
 //Menambah Children pada Main
 const body = document.body
-var playerDiv = document.createElement("div")
+let playerDiv = document.createElement("div")
 playerDiv.setAttribute("class", "player")
 body.appendChild(playerDiv)
 
 //Menambah isi di kotak player
 //Menambah tulisan Player
-var playerInPlayerBox = document.createElement("h2")
-var playerInPlayerBoxText = document.createTextNode("Player")
+let playerInPlayerBox = document.createElement("h2")
+playerInPlayerBox.setAttribute("id", "titlePlayer")
+let playerInPlayerBoxText = document.createTextNode("PLAYER")
 playerInPlayerBox.appendChild(playerInPlayerBoxText)
 playerDiv.appendChild(playerInPlayerBox)
 
 //Menambah Jenis Player dan Keterangan (Health & Position)
 for(player=1; player<2; player++){
     //Membuat Div Wadah jenis Player
-    var jenisPlayerDiv = document.createElement("div")
+    let jenisPlayerDiv = document.createElement("div")
     jenisPlayerDiv.setAttribute("class", `player${player}`)
     playerDiv.appendChild(jenisPlayerDiv)
-    //Membuat isi Wadah Div jenis Player
-    var jenisPlayerInPlayerBox = document.createElement("h4")
-    var jenisPlayerInPlayerBoxText = document.createTextNode(`Player ${player}`)
+    //Membuat logo jenis Player
+    let markerPlayer1 = document.createElement("p")
+    markerPlayer1.setAttribute("class", "marker")
+    jenisPlayerDiv.appendChild(markerPlayer1)
+    //Membuat judul jenis Player
+    let jenisPlayerInPlayerBox = document.createElement("h3")
+    let jenisPlayerInPlayerBoxText = document.createTextNode(`Player ${player}`)
     jenisPlayerInPlayerBox.appendChild(jenisPlayerInPlayerBoxText)
+    jenisPlayerInPlayerBox.setAttribute("id", "titlePlayer1")
     jenisPlayerDiv.appendChild(jenisPlayerInPlayerBox)
     //Membuat Position
-    var positionPlayer = document.createElement("p")
-    var positionPlayerText = document.createTextNode(`Position :`)
+    let positionPlayer = document.createElement("p")
+    let positionPlayerText = document.createTextNode(`Position :`)
     positionPlayer.appendChild(positionPlayerText)
     jenisPlayerDiv.appendChild(positionPlayer)
     //Membuat Health
-    var healthPlayer = document.createElement("p")
-    var healthPlayerText = document.createTextNode(`Health :`)
+    let healthPlayer = document.createElement("p")
+    let healthPlayerText = document.createTextNode(`Health :`)
     healthPlayer.appendChild(healthPlayerText)
     jenisPlayerDiv.appendChild(healthPlayer)
 }
 
 //Menambah div Dadu
-var daduDiv = document.createElement("div")
+let daduDiv = document.createElement("div")
 daduDiv.setAttribute("class", "dadu")
-daduDiv.innerHTML = "<h2>Dadu</h2>"
+playerInPlayerBox.setAttribute("id", "titlePlayer")
+daduDiv.innerHTML = "<h3 class='titleDadu'>Dadu</h3>"
 playerDiv.appendChild(daduDiv)
 
 //Membuat tempat dadu
-var tempatDadu = document.createElement("p")
+let tempatDadu = document.createElement("p")
 tempatDadu.setAttribute("id", "placeholderDadu")
 daduDiv.appendChild(tempatDadu)
 
 //Membuat Roll Dadu
-var buttonDadu = document.createElement("button")
+let buttonDadu = document.createElement("button")
 buttonDadu.setAttribute("id", "dadu")
 buttonDadu.innerHTML = "ROLL DICE"
 daduDiv.appendChild(buttonDadu)
 
 //Mengisi angka pada Roll Dice
-var dadu = {
+let dadu = {
     acak: function(){
         var resultRandom = Math.floor(Math.random() * (6) + 0) + 1;
         return resultRandom;
@@ -123,10 +130,10 @@ var dadu = {
 }
 //Print angka pada layout
 function printDadu(angka){
-    var placeholderDadu = document.getElementById('placeholderDadu');
+    let placeholderDadu = document.getElementById('placeholderDadu');
     placeholderDadu.innerHTML = angka;
 }
-var button = document.getElementById("dadu")
+let button = document.getElementById("dadu")
 button.onclick = function(){
     var hasil = dadu.acak();
     printDadu(hasil);
